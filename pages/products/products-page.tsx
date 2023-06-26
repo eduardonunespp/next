4a8f237c233213c1eeddc387d99747beeab09055
Carlos Eduardo <carlospn200406@gmail.com>
2023-06-26 17:14:34 -0300

    feat: first commit

---
+++ pages/products/products-page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Products: React.FC = () => {
+  return (
+    <>
+      <h2>Sim, está é página de produtos</h2>
+    </>
+  );
+};
+
+export default Products;
---
+++ pages/sim.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sim: React.FC = () => {
+  return (
+    <>
+      <h1>Sim</h1>
+    </>
+  );
+};
+
+export default Sim;
